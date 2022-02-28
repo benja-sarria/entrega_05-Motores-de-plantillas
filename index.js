44a2ca7186f3682baca4356595fdc87b8633275f
Benja Sarría Ferrer <benja.sarria@live.com.ar>
2022-02-27 22:49:04 -0300

    (Fix) - Se eliminaron los motores de plantillas que no correspondian con la rama

--- index.js
+++ index.js
@@ -8,29 +8,9 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 // Templates Engine
-// HANDLEBARS
-/* app.engine(
-    "hbs",
-    engine({
-        // la extensión de los archivos
-        extname: "hbs",
-        defaultLayout: "main.hbs",
-        // indicamos un path absoluto donde estan nuestros layouts y vistas principales
-        layoutsDir: path.resolve(__dirname, "./views/layouts"),
-        // path a nuestras vistas secundarias
-        partialsDir: path.resolve(__dirname, "./views/partials"),
-    })
-);
-app.set("views", "./views");
-app.set("views engine", "hbs"); */
-
 // PUG
-/* app.set("views", "./views/pug");
-app.set("view engine", "pug"); */
-
-// EJS
-app.set("views", "./views/ejs");
-app.set("view engine", "ejs");
+app.set("views", "./views/pug");
+app.set("view engine", "pug");
 
 app.use(express.static("public"));
 
