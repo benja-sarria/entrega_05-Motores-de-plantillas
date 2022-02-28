7c52e8fc95032b575fc270c432d142116fe2d96f
Benja Sarría Ferrer <benja.sarria@live.com.ar>
2022-02-27 22:45:31 -0300

    (Fix) - Se eliminaron los motores de plantillas que no corresponden a esta rama

--- routers/products/products.routes.js
+++ routers/products/products.routes.js
@@ -11,44 +11,6 @@ router.use(productsInstance);
 // Routes
 
 // GET
-// HANDLEBARS
-// /api/products/
-/* router.get("/", async (req, res) => {
-    const allProducts = await req.products.getAllProducts();
-    const formattedProducts = allProducts.map((product) => {
-        return {
-            ...product,
-            price: new Intl.NumberFormat("es-AR", {
-                style: "currency",
-                currency: "ARS",
-            }).format(product.price),
-        };
-    });
-    res.render("index.hbs", {
-        formattedProducts,
-        customstyle: `<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">`,
-        customStyleCss: "<link rel='stylesheet' href='../css/styles.css' />",
-    });
-}); */
-
-// PUG
-// /api/products/
-/* router.get("/", async (req, res) => {
-    const allProducts = await req.products.getAllProducts();
-    const formattedProducts = allProducts.map((product) => {
-        return {
-            ...product,
-            price: new Intl.NumberFormat("es-AR", {
-                style: "currency",
-                currency: "ARS",
-            }).format(product.price),
-        };
-    });
-    res.render("main", {
-        formattedProducts,
-    });
-}); */
-
 // EJS
 // /api/products/
 router.get("/", async (req, res) => {
